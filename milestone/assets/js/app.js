371256f2783f53dfcdb1564c1891128eefa2b4e2
Eoinburke <burkeyeoin45@gmail.com>
2021-05-09 22:20:22 +0000

    save api

--- milestone/assets/js/app.js
+++ milestone/assets/js/app.js
@@ -1,23 +1,21 @@
+const baseURL = 'https://hotels4.p.rapidapi.com/get-meta-data/?rapidapi-key=a31d4df07bmsh96320001ced0244p1169dfjsnd0bf8d0a37ef';
+
+function getData(type, cb) {
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var xhr = new XMLHttpRequest();
-var data;
 
-xhr.open('GET', 'https://hotels4.p.rapidapi.com/get-meta-data/?rapidapi-key=a31d4df07bmsh96320001ced0244p1169dfjsnd0bf8d0a37ef');
+xhr.open('GET', baseURL + type + "/");
 xhr.send();
 
-function setData(jsonData) {
-    data = jsonData;
-    console.log(data);
-}
-
 xhr.onreadystatechange = function() {
-    console.log(this.readyState);
     if(this.readyState == 4 && this.status == 200) {
-        data = this.responseText;
-        setData(JSON.parse(this.responseText));
+       cb(JSON.parse(this.responseText));
     }
 };
+}
 
-setTimeout(function() {
-console.log(data);
-}, 800);
+function writeToDocument(type) {
+getData(type, function(data) {
+document.getElementById('data').innerHTML = data;
+});
+}
