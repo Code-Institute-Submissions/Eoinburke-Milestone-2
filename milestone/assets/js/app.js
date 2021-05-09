e3e7cf4c2600e8420b64e8d2c99b3a6da82d9ae1
Eoinburke <burkeyeoin45@gmail.com>
2021-05-09 21:37:41 +0000

    java api

--- milestone/assets/js/app.js
+++ milestone/assets/js/app.js
@@ -1,10 +1,23 @@
+// Create a request variable and assign a new XMLHttpRequest object to it.
 var xhr = new XMLHttpRequest();
+var data;
 
-xhr.open('GET', 'https://rapidapi.com/');
+xhr.open('GET', 'https://hotels4.p.rapidapi.com/get-meta-data/?rapidapi-key=a31d4df07bmsh96320001ced0244p1169dfjsnd0bf8d0a37ef');
 xhr.send();
 
+function setData(jsonData) {
+    data = jsonData;
+    console.log(data);
+}
+
 xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById('data').innerHTML = this.responseText;
+    console.log(this.readyState);
+    if(this.readyState == 4 && this.status == 200) {
+        data = this.responseText;
+        setData(JSON.parse(this.responseText));
     }
 };
+
+setTimeout(function() {
+console.log(data);
+}, 800);
